4a80595aa189db7bb05450dab0275bc9e52ff70e
Kévin Séguin <kevin_cross_9@hotmail.com>
2018-10-01 12:45:21 +0200

    Move 3 functions in new file (util.go)

---
+++ util.go
@@ -0,0 +1,53 @@
+package main 
+
+import(
+	"os"
+	"fmt"
+
+	"github.com/forj-oss/goforjj"
+	"golang.org/x/sys/unix"
+)
+
+// Linux support only
+func IsWritable(path string) (res bool) {
+	return unix.Access(path, unix.W_OK) == nil
+}
+
+// check path is writable.
+// return false if something is wrong.
+func reqCheckPath(name, path string, ret *goforjj.PluginData) bool {
+
+	if path == "" {
+		ret.ErrorMessage = name + " is empty."
+		return true
+	}
+
+	if _, err := os.Stat(path); err != nil {
+		ret.ErrorMessage = fmt.Sprintf(name+" mounted '%s' is inexistent.", path)
+		return true
+	}
+
+	if !IsWritable(path) {
+		ret.ErrorMessage = fmt.Sprintf(name+" mounted '%s' is NOT writable", path)
+		return true
+	}
+
+	return false
+}
+
+func (gls *GitlabPlugin) verify_req_fails(ret *goforjj.PluginData, check map[string]bool) bool{
+	if v, ok := check["source"]; ok && v {
+		if reqCheckPath("source (forjj-source-mount)", gls.sourceMount, ret){
+			return true
+		}
+	}
+
+	if v, ok := check["token"]; ok && v {
+		if gls.token == ""{
+			ret.ErrorMessage = fmt.Sprint("gitlab token is empty - Required")
+			return true
+		}
+	}
+
+	return false
+}
